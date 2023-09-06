2633e88e4635ffa8cb0e13a5f089d88e4c893a0f
Patrick Eakin <patrickeakin@protonmail.com>
2023-09-06 19:49:12 -0400

    remove semicolon

--- tests/asana.spec.js
+++ tests/asana.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 const login = require('../scripts/login.js')
 const testCases = JSON.parse(JSON.stringify(require('./testCases.json')))
 
@@ -7,7 +7,7 @@ test.beforeEach(login)
 
 testCases.forEach(data => {
     test(`${data.name}`, async ({ page }) => {
-        await test.step('Click on project in sidebar', async () => {
+        await test.step('Click project in sidebar', async () => {
             await page.getByLabel(data.leftNav).click()
         })
         await expect(
